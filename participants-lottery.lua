47fdec1a09619cc050dd9e76c1ee021386a55722
areee <10089872+areee@users.noreply.github.com>
2021-04-25 20:17:42 +0300

    Add 'RunLottery' method

--- participants-lottery.lua
+++ participants-lottery.lua
@@ -4,6 +4,45 @@ participants_text = ""
 
 hotkey_id     = obs.OBS_INVALID_HOTKEY_ID
 
+function RunLottery(Names)
+    string.split =
+        function(str, pattern) -- Source for this string splitting method: http://lua-users.org/wiki/SplitJoin
+            pattern = pattern or "[^%s]+"
+            if pattern:len() == 0 then pattern = "[^%s]+" end
+            local parts = {__index = table.insert}
+            setmetatable(parts, parts)
+            str:gsub(pattern, parts)
+            setmetatable(parts, nil)
+            parts.__index = nil
+            return parts
+        end
+
+    NameList = Names:split("[^,%s]+")
+    OrderList = {}
+
+    function Contains(Array, Object)
+        for i = 1, #Array do
+            Val = Array[i]
+            if (Val == Object) then return true end
+        end
+        return false
+    end
+
+    repeat
+        RandomNumber = math.random(1, #NameList)
+        if not Contains(OrderList, RandomNumber) then
+            table.insert(OrderList, RandomNumber)
+        end
+    until #OrderList == #NameList
+
+    ReturnedString = ""
+
+    for i = 1, #OrderList do
+        ReturnedString = ReturnedString .. NameList[OrderList[i]] .. "\n"
+    end
+    return ReturnedString
+end
+
 -- =========================================================================
 
 function script_description()
