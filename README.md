ce6d30415c6c65a340316f293cb6daf24f9c0461
areee <10089872+areee@users.noreply.github.com>
2021-04-25 19:56:11 +0300

    Add the lua script file & update the readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # obs-participants-lottery
-A lottery script for OBS
+> A lottery script for OBS
+
+Made by Arttu Ylhävuori, 2021
+
+- The source code is inspired by [the countdown script of OBS Studio](https://github.com/obsproject/obs-studio/blob/master/UI/frontend-plugins/frontend-tools/data/scripts/countdown.lua)
---
+++ participants-lottery.lua
@@ -0,0 +1,11 @@
+obs           = obslua
+source_name   = ""
+participants_text = ""
+
+hotkey_id     = obs.OBS_INVALID_HOTKEY_ID
+
+-- =========================================================================
+
+function script_description()
+    return "Lottery of meeting participants in random order.\n\nMade by Arttu Ylhävuori, 2021"
+end
