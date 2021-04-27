39a88c138565099be754b1633d0b96845f95f6ae
areee <10089872+areee@users.noreply.github.com>
2021-04-27 19:49:26 +0300

    Fix 'Global variable in lowercase initial' warnings

--- participants-lottery.lua
+++ participants-lottery.lua
@@ -1,13 +1,12 @@
-obs                 = obslua
-source_name         = ""
-last_text           = ""
-participants_text   = ""
-activated           = false
-items_per_row       = 0
+Obs                 = obslua
+Source_name         = ""
+Last_text           = ""
+Participants_text   = ""
+Activated           = false
+Items_per_row       = 0
+Hotkey_id           = Obs.OBS_INVALID_HOTKEY_ID
 
-hotkey_id           = obs.OBS_INVALID_HOTKEY_ID
-
-function run_lottery()
+function Run_lottery()
     string.split =
         function(str, pattern) -- Source for this string splitting method: http://lua-users.org/wiki/SplitJoin
             pattern = pattern or "[^%s]+"
@@ -20,7 +19,7 @@ function run_lottery()
             return parts
         end
 
-    NameList = participants_text:split("[^,%s]+")
+    NameList = Participants_text:split("[^,%s]+")
     OrderList = {}
 
     function Contains(Array, Object)
@@ -43,60 +42,60 @@ function run_lottery()
     for i = 1, #OrderList do
         text = text .. NameList[OrderList[i]]
 
-        if i % items_per_row == 0 then
+        if i % Items_per_row == 0 then
             text = text .. "\n"
         elseif i ~= #OrderList then
             text = text .. ","
         end
     end
 
-    if text ~= last_text then
-        local source = obs.obs_get_source_by_name(source_name)
+    if text ~= Last_text then
+        local source = Obs.obs_get_source_by_name(Source_name)
         if source ~= nil then
-            local settings = obs.obs_data_create()
-            obs.obs_data_set_string(settings, "text", text)
-            obs.obs_source_update(source, settings)
-            obs.obs_data_release(settings)
-            obs.obs_source_release(source)
+            local settings = Obs.obs_data_create()
+            Obs.obs_data_set_string(settings, "text", text)
+            Obs.obs_source_update(source, settings)
+            Obs.obs_data_release(settings)
+            Obs.obs_source_release(source)
         end
     end
-    last_text = text
+    Last_text = text
 end
 
-function activate(activating)
-    if activated == activating then return end
+function Activate(activating)
+    if Activated == activating then return end
 
-    activated = activating
+    Activated = activating
 
-    if activating then run_lottery() end
+    if activating then Run_lottery() end
 end
 
-function activate_signal(cd, activating)
-    local source = obs.calldata_source(cd, "source")
+function Activate_signal(cd, activating)
+    local source = Obs.calldata_source(cd, "source")
     if source ~= nil then
-        local name = obs.obs_source_get_name(source)
-        if (name == source_name) then activate(activating) end
+        local name = Obs.obs_source_get_name(source)
+        if (name == Source_name) then Activate(activating) end
     end
 end
 
-function source_activated(cd) activate_signal(cd, true) end
+function Source_activated(cd) Activate_signal(cd, true) end
 
-function source_deactivated(cd) activate_signal(cd, false) end
+function Source_deactivated(cd) Activate_signal(cd, false) end
 
-function start_again(pressed)
+function Start_again(pressed)
     if not pressed then return end
 
-    activate(false)
-    local source = obs.obs_get_source_by_name(source_name)
+    Activate(false)
+    local source = Obs.obs_get_source_by_name(Source_name)
     if source ~= nil then
-        local active = obs.obs_source_active(source)
-        obs.obs_source_release(source)
-        activate(active)
+        local active = Obs.obs_source_active(source)
+        Obs.obs_source_release(source)
+        Activate(active)
     end
 end
 
-function start_again_button_clicked(props, p)
-    start_again(true)
+function Start_again_button_clicked(props, p)
+    Start_again(true)
     return false
 end
 
@@ -108,63 +107,63 @@ function script_description()
 end
 
 function script_properties()
-    local props = obs.obs_properties_create()
+    local props = Obs.obs_properties_create()
 
-    local p = obs.obs_properties_add_list(props, "source", "Text Source",
-                                          obs.OBS_COMBO_TYPE_EDITABLE,
-                                          obs.OBS_COMBO_FORMAT_STRING)
-    local sources = obs.obs_enum_sources()
+    local p = Obs.obs_properties_add_list(props, "source", "Text Source",
+                                          Obs.OBS_COMBO_TYPE_EDITABLE,
+                                          Obs.OBS_COMBO_FORMAT_STRING)
+    local sources = Obs.obs_enum_sources()
     if sources ~= nil then
         for _, source in ipairs(sources) do
-            source_id = obs.obs_source_get_unversioned_id(source)
-            if source_id == "text_gdiplus" or source_id == "text_ft2_source" then
-                local name = obs.obs_source_get_name(source)
-                obs.obs_property_list_add_string(p, name, name)
+            Source_id = Obs.obs_source_get_unversioned_id(source)
+            if Source_id == "text_gdiplus" or Source_id == "text_ft2_source" then
+                local name = Obs.obs_source_get_name(source)
+                Obs.obs_property_list_add_string(p, name, name)
             end
         end
     end
-    obs.source_list_release(sources)
+    Obs.source_list_release(sources)
 
-    obs.obs_properties_add_text(props, "participants_text", "Participants",
-                                obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_button(props, "start_again_button", "Start again",
-                                  start_again_button_clicked)
-    obs.obs_properties_add_int(props, "items", "Items per row", 1, 100000, 1)
+    Obs.obs_properties_add_text(props, "participants_text", "Participants",
+                                Obs.OBS_TEXT_DEFAULT)
+    Obs.obs_properties_add_button(props, "start_again_button", "Start again",
+                                  Start_again_button_clicked)
+    Obs.obs_properties_add_int(props, "items", "Items per row", 1, 100000, 1)
 
     return props
 end
 
 function script_update(settings)
-    activate(false)
+    Activate(false)
 
-    source_name = obs.obs_data_get_string(settings, "source")
-    participants_text = obs.obs_data_get_string(settings, "participants_text")
-    items_per_row = obs.obs_data_get_int(settings, "items")
+    Source_name = Obs.obs_data_get_string(settings, "source")
+    Participants_text = Obs.obs_data_get_string(settings, "participants_text")
+    Items_per_row = Obs.obs_data_get_int(settings, "items")
 
-    start_again(true)
+    Start_again(true)
 end
 
 function script_defaults(settings)
-    obs.obs_data_set_default_string(settings, "participants_text",
+    Obs.obs_data_set_default_string(settings, "participants_text",
                                     "Mike, Catherine, Tim, Julia")
-    obs.obs_data_set_default_int(settings, "items", 10)
+    Obs.obs_data_set_default_int(settings, "items", 10)
 end
 
 function script_save(settings)
-    local hotkey_save_array = obs.obs_hotkey_save(hotkey_id)
-    obs.obs_data_set_array(settings, "start_again_hotkey", hotkey_save_array)
-    obs.obs_data_array_release(hotkey_save_array)
+    local hotkey_save_array = Obs.obs_hotkey_save(Hotkey_id)
+    Obs.obs_data_set_array(settings, "start_again_hotkey", hotkey_save_array)
+    Obs.obs_data_array_release(hotkey_save_array)
 end
 
 function script_load(settings)
-    local sh = obs.obs_get_signal_handler()
-    obs.signal_handler_connect(sh, "source_activate", source_activated)
-    obs.signal_handler_connect(sh, "source_deactivate", source_deactivated)
+    local sh = Obs.obs_get_signal_handler()
+    Obs.signal_handler_connect(sh, "source_activate", Source_activated)
+    Obs.signal_handler_connect(sh, "source_deactivate", Source_deactivated)
 
-    hotkey_id = obs.obs_hotkey_register_frontend("start_again_timer_thingy",
-                                                 "Start again", start_again)
-    local hotkey_save_array = obs.obs_data_get_array(settings,
+    Hotkey_id = Obs.obs_hotkey_register_frontend("start_again_timer_thingy",
+                                                 "Start again", Start_again)
+    local hotkey_save_array = Obs.obs_data_get_array(settings,
                                                      "start_again_hotkey")
-    obs.obs_hotkey_load(hotkey_id, hotkey_save_array)
-    obs.obs_data_array_release(hotkey_save_array)
+    Obs.obs_hotkey_load(Hotkey_id, hotkey_save_array)
+    Obs.obs_data_array_release(hotkey_save_array)
 end
